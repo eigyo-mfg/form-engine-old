33f81be573e71d352a81648926c8f8097aa447ad
nishishimamoto <nishishimamoto@sales-bank.com>
2023-08-15 09:42:02 +0900

    GPT-4のプロンプト修正。郵便番号、電話番号が分かれていても正しく入力できた。

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // 対象のURLの定義
-const url = 'https://estem-web.com/contact.html';
+const url = 'https://www.arduc.co.jp/contact/';
 
 // メインの非同期関数の定義
 async function run() {
@@ -138,11 +138,7 @@ async function run() {
                 } else {
                     fields.push({ name: value, value: name, type: type, label: label });
                 }
-            };
-            
-            // 以降のコードは変更なし
-            
-            
+            };            
             
             // input fields
             $('input[type="text"], input[type="email"], input[type="date"], input[type="month"], input[type="number"], input[type="tel"], input[type="time"], input[type="url"], input[type="week"], textarea').each(function() {
@@ -172,63 +168,69 @@ async function run() {
             console.log(JSON.stringify({ fields, submit }, null, 2));
                         
             const dataToSend = {
-                企業名: "営業製作所株式会社",
-                担当者名: "安田　美佳",
-                ふりがな担当者名: "やすだ　みか",
-                漢字性:"安田",
-                漢字名:"美佳",
-                ふりがな性: "やすだ",
-                ふりがな名: "みか",
-                メール: "m.yasuda@sales-bank.com",
-                電話: "06-6136-8027",
-                郵便番号: "550-0002",
-                住所: "大阪府大阪市西区江戸堀1-22-38　三洋ビル501",
-                生年月日:"1992年4月14日",
-                返信方法: "メール",
-                問い合わせ分類: "サービスについて",
-                部署:"営業部",
-                役職:"主査",
-                件名:"【製造業7,000名の担当者から廃材回収のニーズを頂戴しております】",
-                問い合わせ内容:
-                "代表者様 \nお世話になります。\n営業製作所の安田と申します。\n製造業の担当者7,000名から廃材回収に関するニーズを頂戴しております\n具体的なニーズの有無まで調査行い、ご紹介が可能ですのでご連絡させていただきました。\n弊社は、製造業に特化した事業を展開しており、 サービスリリース2年で500社の企業様にご活用いただいております。\n貴社の回収しやすい【材質】【大きさ】【形状】【重量】を満たす、取引先を発掘することが可能です。 \n同業他社での実績や貴社に合致したレポートをご用意しておりますので、ご興味をお持ち頂ける場合はお電話にて詳細をお伝えします。\n 下記メールアドレスにお電話可能な日時をお送りくださいませ。\n ■メールアドレス m.yasuda@sales-bank.com \n■弊社パンフレット https://tinyurl.com/239r55dc \nそれではご連絡お待ちしております。"
+企業名: "営業製作所株式会社",
+担当者名: "安田　美佳",
+ふりがな担当者名: "やすだ　みか",
+漢字性:"安田",
+漢字名:"美佳",
+ふりがな性: "やすだ",
+ふりがな名: "みか",
+メール: "m.yasuda@sales-bank.com",
+電話: "06-6136-8027",
+電話上:"06",
+電話中:"6136",
+電話下:"8027",
+郵便番号: "550-0002",
+郵便番号上:"550",
+郵便番号下:"0002",
+住所: "大阪府大阪市西区江戸堀1-22-38　三洋ビル501",
+生年月日:"1992年4月14日",
+返信方法: "メール",
+問い合わせ分類: "サービスについて",
+部署:"営業部",
+役職:"主査",
+件名:"【製造業7,000名の担当者から廃材回収のニーズを頂戴しております】",
+問い合わせ内容:
+"代表者様 \nお世話になります。\n営業製作所の安田と申します。\n製造業の担当者7,000名から廃材回収に関するニーズを頂戴しております\n具体的なニーズの有無まで調査行い、ご紹介が可能ですのでご連絡させていただきました。\n弊社は、製造業に特化した事業を展開しており、 サービスリリース2年で500社の企業様にご活用いただいております。\n貴社の回収しやすい【材質】【大きさ】【形状】【重量】を満たす、取引先を発掘することが可能です。 \n同業他社での実績や貴社に合致したレポートをご用意しておりますので、ご興味をお持ち頂ける場合はお電話にて詳細をお伝えします。\n 下記メールアドレスにお電話可能な日時をお送りくださいませ。\n ■メールアドレス m.yasuda@sales-bank.com \n■弊社パンフレット https://tinyurl.com/239r55dc \nそれではご連絡お待ちしております。"
             };
+            dataToSend.問い合わせ内容 = dataToSend.問い合わせ内容.substring(0, 20);
 
             async function mapFieldToData(fields, dataToSend) {
-                const fieldsJsonString = JSON.stringify({ fields: fields }, null, 2);
+                const fieldsJsonString = JSON.stringify(fields);
                 const promptContent = `
-                Analyze the following fields and data, and map the field names with the corresponding data. If a field has multiple "selectedValue" options, choose the one that corresponds to the key in the "dataToSend". If a perfect match is not found, choose a default value to ensure submission.
+Fields to analyze:
+${fieldsJsonString}
 
-                Fields to analyze:
-                ${fieldsJsonString}
-                
-                Data to analyze:
-                ${JSON.stringify(dataToSend, null, 2)}
-                
-                Provide the analysis result in the following JSON format:
-                {
-                    "fields": [
-                        // For text, email, textarea fields: {"name": "dataToSend's key", "value": "field attribute name", "type": "field's type"}
-                        // For radio, select fields: {"name": "dataToSend's key", "value": "field attribute name", "type": "field's type", "selectedValue": "chosen value"}
-                    ],
-                    "submit": "submit button's selector" // e.g., button[name="submitName"]
-                }
-                
-                Map all text, email, textarea fields accurately.
-                For radio or checkbox or select, specify the selected value based on the given information.
-                Identify the submit button's selector precisely.
-                Inquiry content is likely to be analyzed as a textarea field.
-                Not all content in dataToSend needs to be used.
-                `;
-                
+dataToSend to analyze:
+${JSON.stringify(dataToSend, null, 2)}
+
+Analyze the above fields and data (dataToSend), and map the field names with the corresponding data. Select the field names for all text, email, date, month, number, tel, time, url, week, and textarea fields from the keys in dataToSend. The value and type attributes of the original field must remain intact.
+
+Provide the analysis result in the following JSON format:
+{
+    "fields": [
+        // For text, email, tel, url, and textarea fields: {"name": "key from dataToSend", "value": "field's value", "type": "field's type"}
+        // For radio, checkbox, or select fields: {"name": "key from dataToSend", "value": "field attribute name", "type": "field's type", "values": "chosen value"}
+    ],
+    "submit": "submit button's selector" // e.g., button[name="submitName"]
+}
+
+If a field has multiple "values" options, choose the one that corresponds to the key in "dataToSend". If a perfect match is not found, choose the last value of the applicable values to ensure submission.
+
+For radio, checkbox, or select, specify the selected value based on the given information.
+Identify the submit button's selector precisely.
+The inquiry content is likely to be analyzed as a textarea field.
+It is not necessary to use all the content in dataToSend.
+`;                         
             
                 console.log("Prompt Content:", promptContent);
             
                 const completion = await openai.createChatCompletion({
                     model: "gpt-4",
                     messages: [
-                        {"role": "system", "content": "You are one of the world's leading engineers, specializing in HTML analysis."},
+                        {"role": "system", "content": "You are a professional who deeply understands the structure of HTML and is proficient in both English and Japanese. You are capable of minimizing mistakes, carefully verifying multiple times, and handling tasks with precision."},
                         {"role": "user", "content": promptContent}
-                    ]
+                    ]                    
                 });            
                 const mappedName = completion.data.choices[0].message.content;
                 console.log("Mapped Name:", mappedName); 
