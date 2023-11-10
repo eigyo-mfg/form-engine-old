8d1c242112f7988db3150d4f16fe97bbcaab441d
Shinya Nishihara <b1012181@gmail.com>
2023-11-11 03:34:04 +0900

    JSONにコメントがある場合は削除するように修正

--- src/utils/string.js
+++ src/utils/string.js
@@ -3,8 +3,10 @@ function extractJson(str) {
     const endIdx = str.lastIndexOf('}');
     if (startIdx !== -1 && endIdx !== -1) {
         const jsonString = str.slice(startIdx, endIdx + 1);
+        // コメントを削除（//から始まる行を削除）
+        const jsonWithoutComments = jsonString.replace(/\/\/.*$/gm, '');
         try {
-            return JSON.parse(jsonString);
+            return JSON.parse(jsonWithoutComments);
         } catch (e) {
             console.error(`Failed to parse JSON: ${e}`);
         }
