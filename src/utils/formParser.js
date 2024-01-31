75bfb03bac31f95e99e2678e7cb67052786cb567
Shinya Nishihara <b1012181@gmail.com>
2024-01-31 12:42:48 +0900

    hidden要素に隠れてる場合の対応

--- src/utils/formParser.js
+++ src/utils/formParser.js
@@ -183,7 +183,9 @@ function mergeFields(fields) {
 
 function getSubmitElement(formHtml) {
   const $ = cheerio.load(formHtml);
-  let submitEl = $('button[type="submit"], input[type="submit"]');
+  let submitEl = $('button[type="submit"], input[type="submit"]').filter(function() {
+    return !$(this).parent().is(':hidden');
+  });
   if (submitEl.length > 0) {
     return submitEl;
   }
